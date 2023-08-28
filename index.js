85426f9cc4c1616aa58812b69551efe39477bceb
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-08-29 01:55:00 +0600

    update data added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-//
+//Middleware
 app.use(express.json());
 app.use(cors());
 
@@ -40,6 +40,14 @@ async function run() {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    //Update Data
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
     });
 
     //post data
